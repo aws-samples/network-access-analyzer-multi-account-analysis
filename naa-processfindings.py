4fe07c9d2a4e391212b1280f2f8f6fd86ec286f2
Mike Virgilio <micvirgi@amazon.com>
2023-01-19 22:33:35 -0600

    minor update

--- naa-processfindings.py
+++ naa-processfindings.py
@@ -1,5 +1,5 @@
 # This sample, non-production-ready python script process network analyzer results of a specific scope and outputs an csv for further processing and analysis.
-# © 2022 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
+# © 2023 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
 # This AWS Content is provided subject to the terms of the AWS Customer Agreement available at
 # http://aws.amazon.com/agreement or other written agreement between Customer and either
 # Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
